8106c0258657279d81fbaaf459720268766733b5
Dustin <135719141+DustinTurska@users.noreply.github.com>
2024-07-28 10:37:43 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,14 +108,14 @@ export default function Home() {
 function Header() {
   return (
     <header className="flex flex-col items-center mb-20 md:mb-20">
-      <Image
+      {/* <Image
         src={thirdwebIcon}
         alt=""
         className="size-[150px] md:size-[150px]"
         style={{
           filter: "drop-shadow(0px 0px 24px #a726a9a8)",
         }}
-      />
+      /> */}
 
       <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
         OP Superchain App Accelerator
