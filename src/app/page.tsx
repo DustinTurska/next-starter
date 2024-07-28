23aad1c263f7fd9015ea70452072978f8303ae2b
Dustin <135719141+DustinTurska@users.noreply.github.com>
2024-07-28 10:28:43 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { optimism } from "thirdweb/chains";
 import { createWallet, inAppWallet } from "thirdweb/wallets";
 import { useState } from 'react';
 import { Confetti } from "./Confetti";
+import { MediaRenderer } from "thirdweb/react";
 
 const wallets = [
   createWallet("io.metamask"),
@@ -35,30 +36,42 @@ export default function Home() {
       <div className="py-20">
         <Header />
 
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            accountAbstraction={{
-              chain: optimism,
-              factoryAddress: "0x85e23b94e7F5E9cC1fF78BCe78cfb15B81f0DF00",
-              gasless: true,
-            }}
-          />
-        </div>
-
-        <div className="flex justify-center mb-20">
-          <ClaimButton
-            contractAddress="0x2992e480001AfA6097a4BC7bB8c02d7df819d4cE"
-            chain={optimism}
-            client={client}
-            onTransactionSent={handleTransactionSent}
-            claimParams={{
-              type: "ERC721",
-              quantity: 1n,
-            }}
-          >
-            Claim NFT
-          </ClaimButton>
+        <div className="flex flex-col md:flex-row justify-between items-center mb-20">
+          <div className="w-full md:w-1/2 mb-10 md:mb-0">
+            <MediaRenderer
+              src="ipfs://QmZ1512rWfso1iUh2UkK5LUjw73zCHsxc5RXnsh6NfJo63/TreasureChests.png"
+              alt="Example NFT image"
+              width="100%"
+              height="auto"
+              client={client}
+            />
+          </div>
+          <div className="w-full md:w-1/2 flex flex-col items-center">
+            <div className="mb-10">
+              <ConnectButton
+                client={client}
+                accountAbstraction={{
+                  chain: optimism,
+                  factoryAddress: "0x85e23b94e7F5E9cC1fF78BCe78cfb15B81f0DF00",
+                  gasless: true,
+                }}
+              />
+            </div>
+            <div>
+              <ClaimButton
+                contractAddress="0x2992e480001AfA6097a4BC7bB8c02d7df819d4cE"
+                chain={optimism}
+                client={client}
+                onTransactionSent={handleTransactionSent}
+                claimParams={{
+                  type: "ERC721",
+                  quantity: 1n,
+                }}
+              >
+                Claim NFT
+              </ClaimButton>
+            </div>
+          </div>
         </div>
 
         <Confetti fire={showConfetti} />
@@ -80,8 +93,8 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-500 hover:text-blue-600"
-              >
-                {openSea}
+            >
+              {openSea}
             </a>
           </div>
         )}
